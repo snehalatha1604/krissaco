9fd3a7b2f777977fa22876c479b7379ee0426659
Pavan G <gollapalli.shanmukpavan@gmail.com>
2025-02-10 23:34:36 +0530

    index.js update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import Products from "./screens/Products";
+import Services from "./screens/Services";
+import Careers from "./screens/Careers";
+// import ErrorPage from "./ErrorPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <ErrorPage />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/services",
+    element: <Services />,
+  },
+  {
+    path: "/careers",
+    element: <Careers />,
+    // errorElement: <ErrorPage />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );

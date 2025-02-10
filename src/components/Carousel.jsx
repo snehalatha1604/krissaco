7ffe109a27a1793412175a39fa10a301cc74ba03
Pavan G <gollapalli.shanmukpavan@gmail.com>
2025-02-10 22:57:20 +0530

    navbar and main carousel completed

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect, useCallback } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+const Carousel = () => {
+  const slides = [
+    {
+      src: "/img/slide2.png",
+      alt: "Slide 2",
+      text: "When rare coffee is bought to your kitchen",
+    },
+    {
+      src: "/img/slide3.png",
+      alt: "Slide 3",
+      text: "Enjoy a classic taste for Modern coffee lovers",
+    },
+    {
+      src: "/img/slide1.png",
+      alt: "Slide 1",
+      text: "Streamline your food processing business the Sleek way",
+    },
+    {
+      src: "/img/slide4.png",
+      alt: "Slide 4",
+      text: "We bottled something that is more than a filter coffee",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = useCallback(() => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+  }, [slides.length]);
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + slides.length) % slides.length
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 2000);
+    return () => clearInterval(interval);
+  }, [nextSlide]);
+  return (
+    <div className="relative overflow-hidden w-full ">
+      {/* Slides Wrapper */}
+      <div
+        className="flex transition-transform duration-700 ease-in-out"
+        style={{
+          transform: `translateX(-${currentIndex * 100}%)`,
+        }}
+      >
+        {slides.map((slide, index) => (
+          <div
+            key={index}
+            className="w-full md:h-[calc(100vh-65px)] h-full flex-shrink-0 relative"
+          >
+            <img
+              src={slide.src}
+              alt={slide.alt}
+              className="w-full h-full object-cover"
+            />
+
+            <h2 className="absolute bottom-1 md:bottom-24 md:left-24 left-5 text-white md:text-6xl text-2xl font-bold px-4 py-2 bg-transparent rounded-md text-shadow">
+              {slide.text}
+            </h2>
+          </div>
+        ))}
+      </div>
+
+      {/* Left Control */}
+      <button
+        onClick={prevSlide}
+        className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-3 rounded-full"
+      >
+        <FaChevronLeft size={24} />
+      </button>
+
+      {/* Right Control */}
+      <button
+        onClick={nextSlide}
+        className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-3 rounded-full"
+      >
+        <FaChevronRight size={24} />
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
+import { FaBox } from "react-icons/fa";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { IoCloud } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+  const [MenuToggle, setMenuToggle] = useState(false);
+  return (
+    <nav className=" bg-white border-b border-gray-200 shadow-sm">
+      <div className="px-4 md:mx-8 lg:mx-14 py-3 flex items-center justify-between">
+        {/* Logo */}
+        <Link to="/">
+          <div className="flex items-center">
+            <img src={logo} alt="CoachOn Logo" className="md:h-10 h-8" />
+          </div>
+        </Link>
+        {/*Small Screen Navbar Items */}
+        <div className="lg:hidden block">
+          {MenuToggle ? (
+            <div className="scale-up-hor-right fixed top-0 right-0 h-fit bg-white shadow-lg">
+              <div className="flex items-center justify-between px-4 md:py-5 py-4 border-b">
+                <span className="text-lg font-bold">Menu</span>
+                <IoMdClose
+                  className="size-[1.5rem]"
+                  onClick={() => setMenuToggle(false)}
+                />
+              </div>
+              {/* Navbar Items */}
+              <ul className="p-10 space-y-8">
+                {/* Premium */}
+                <div>
+                  <Link
+                    to="/"
+                    className="text-gray-600 hover:text-black text-base flex items-center"
+                  >
+                    <FaBox color="green" />
+                    <span className="font-medium ml-2">Premium</span>
+                  </Link>
+                </div>
+                {/* Services */}
+                <div>
+                  <Link
+                    to="/"
+                    className="text-gray-600 hover:text-black text-base flex items-center"
+                  >
+                    <IoCloud color="green" size={20} />
+                    <span className="font-medium ml-2">Services</span>
+                  </Link>
+                </div>
+                {/* Careers */}
+                <div>
+                  <Link
+                    to="/"
+                    className="text-gray-600 hover:text-black text-base flex items-center"
+                  >
+                    <FaPeopleGroup color="green" size={22} />
+                    <span className="font-medium ml-2">Careers</span>
+                  </Link>
+                </div>
+              </ul>
+            </div>
+          ) : (
+            <CiMenuBurger
+              className="size-[1.35rem]"
+              onClick={() => setMenuToggle(true)}
+            />
+          )}
+        </div>
+        {/* Navbar Items */}
+        <div className="space-x-8 lg:flex hidden items-center">
+          {/* Premium */}
+          <div>
+            <Link
+              to="/"
+              className="text-gray-600 hover:text-black text-base flex items-center"
+            >
+              <FaBox color="green" />
+              <span className="font-medium ml-2">Premium</span>
+            </Link>
+          </div>
+          {/* Services */}
+          <div>
+            <Link
+              to="/"
+              className="text-gray-600 hover:text-black text-base flex items-center"
+            >
+              <IoCloud color="green" size={20} />
+              <span className="font-medium ml-2">Services</span>
+            </Link>
+          </div>
+          {/* Careers */}
+          <div>
+            <Link
+              to="/"
+              className="text-gray-600 hover:text-black text-base flex items-center"
+            >
+              <FaPeopleGroup color="green" size={22} />
+              <span className="font-medium ml-2">Careers</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
